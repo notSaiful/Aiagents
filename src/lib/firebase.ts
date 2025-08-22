c6bf48c2b549f01f7b7153d28c0b8a9b5b5cb91a
Saiful Haque <saifulbusiness47@gmail.com>
2025-08-22 08:54:52 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,16 @@
+
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "gemini-notes-kwn2b",
+  "appId": "1:818945879107:web:9f9c94f8cd364ec44078f6",
+  "storageBucket": "gemini-notes-kwn2b.firebasestorage.app",
+  "apiKey": "AIzaSyBbQt8GbMEAojJaFfVKGUGBhm64SUNFpNQ",
+  "authDomain": "gemini-notes-kwn2b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "818945879107"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
