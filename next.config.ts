b1807b36c2e87a673df7f784f7f71e3e97b9c465
Saiful Haque <saifulbusiness47@gmail.com>
2025-09-06 14:04:13 +0000

    increase the ocr upload length

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
   images: {
     remotePatterns: [
       {
